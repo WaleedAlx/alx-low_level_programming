d838a07fd7ffcd922c1a299340f775cedd9966ee
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-10 21:08:05 -0700

    5th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C program to prints string using puts
+ *
+ * Return: Always return 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return(0)
+}
