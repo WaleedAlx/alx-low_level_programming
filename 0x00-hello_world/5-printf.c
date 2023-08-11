90cf864a90e8ad31742ec06fc36d397f5b8a5e18
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-10 21:29:31 -0700

    6th commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Enty point
  * 
  * Description : C program to print using printf
  *
  * Return : Always return 0
- * */
+*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
