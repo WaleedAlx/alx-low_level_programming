6cf69bcbee8e16826059c321c7241e373b98ae9a
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-22 15:12:01 -0700

    updating comments

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_rev - Function that prints a string, in reverse, followed by a new line
+ * print_rev - Function that prints a string
+ * in reverse, followed by a new line
  * @s: string parameter
  * return: return 0 (Success)
  */
