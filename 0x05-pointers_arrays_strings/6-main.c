8e605425fb8ec1ddc3e0566574e432dad6a6c2bc
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-22 15:29:03 -0700

    6-Puts2

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Testing puts2 function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
