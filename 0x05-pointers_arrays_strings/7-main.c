44bf4dbb26c62e9d7688183e630d611b31327146
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-22 15:36:36 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Testing puts_half function
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
