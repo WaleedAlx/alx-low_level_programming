6dd3cafcfdd4524fb0955ae75e9601a05a8701f9
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-08-31 11:58:29 -0700

    ./2-strlen_recursion

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Testing _strlen_recursion function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Function that returns
+ * the length of a string.
+ * @s: The string parameter
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
+}
