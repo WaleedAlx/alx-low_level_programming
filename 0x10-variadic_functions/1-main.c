60e844cebdf694a5fc74a3d337d87ce00144e84e
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-09-15 14:28:38 -0700

    Task 2

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - Entry point
+ * Description: Testing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
