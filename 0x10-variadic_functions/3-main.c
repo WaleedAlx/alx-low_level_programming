6e20c28f2efa27e732c64321011b7718a78810fe
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-09-15 14:37:48 -0700

    Task 3

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - Entry point
+ * Description: Testing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_all("ceis", 'B', 3, "stSchool");
+	return (0);
+}
