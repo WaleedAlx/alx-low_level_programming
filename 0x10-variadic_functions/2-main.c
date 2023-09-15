6a5fdb211e0cca95340e6cd83a80b71b45f924f2
WaleedAli <waleedmohamedbadelsalamali@gmail.com>
2023-09-15 14:33:44 -0700

    Task2

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - Entry point
+ * Description: Testing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
